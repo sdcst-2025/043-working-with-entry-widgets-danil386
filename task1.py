6722b2d86cd30abcceb0058ac59c22a8728d32d8
danil386 <danielal386@deltalearns.ca>
2025-04-28 11:34:42 -0700

    task 1 and task 2 done

--- task1.py
+++ task1.py
@@ -7,3 +7,49 @@ Create entry widets to allow user to enter their:
 
 Create a button so that when they click on the button, it states all of the information in a 4th entry widget
 """
+
+import tkinter as tk 
+
+
+win = tk.Tk()
+
+
+def clickFunction(event):
+    name = e1.get()
+    name = str(name)
+    number = e2.get()
+    number = str(number)
+    grade = e3.get()
+    grade = str(grade)
+
+    info = f"{name}, {number}, {grade}"
+    a_entry.insert(0,info)
+
+
+l1 = tk.Label(win, text="Enter your name:")
+e1 = tk.Entry(win, width=20)
+l2 = tk.Label(win, text="Enter your student number:")
+e2 = tk.Entry(win, width=20)
+l3 = tk.Label(win, text="Enter your grade:")
+e3 = tk.Entry(win, width=20)
+b1 = tk.Button(win, text="Click to see your information")
+
+b1.bind("<Button>",clickFunction)
+
+a_label = tk.Label(win, text="You are:")
+a_entry = tk.Entry(win, width=20)
+
+l1.grid(row=1,column=1)
+e1.grid(row=1,column=2)
+l2.grid(row=2,column=1)
+e2.grid(row=2,column=2)
+l3.grid(row=3,column=1)
+e3.grid(row=3,column=2)
+b1.grid(row=4, column=1, columnspan=2)
+a_label.grid(row=5,column=1)
+a_entry.grid(row=5,column=2)
+
+
+
+
+win.mainloop()
--- task2.py
+++ task2.py
@@ -6,3 +6,43 @@ The first 2 entry widgets allow the user to enter in the 2 short sides of a righ
 When the button is clicked, calculate the length of the hypotenuse and display it in the 3rd entry widget.
 Any labels you need for instruction are optional.
 """
+
+import tkinter as tk 
+
+
+win = tk.Tk()
+
+
+def clickFunction(event):
+    side1 = e1.get()
+    side1 = float(side1)
+    side2 = e2.get()
+    side2 = float(side2)
+
+    hypotenuse= ((side1**2)+(side2**2))**0.5
+    a_entry.insert(0,hypotenuse)
+
+
+l1 = tk.Label(win, text="Enter a value for a side length:")
+e1 = tk.Entry(win, width=20)
+l2 = tk.Label(win, text="Enter another value for a side length:")
+e2 = tk.Entry(win, width=20)
+b1 = tk.Button(win, text="Click to find your hypotenuse")
+
+b1.bind("<Button>",clickFunction)
+
+a_label = tk.Label(win, text="Your hypotenuse has a value of:")
+a_entry = tk.Entry(win, width=20)
+
+l1.grid(row=1,column=1)
+e1.grid(row=1,column=2)
+l2.grid(row=2,column=1)
+e2.grid(row=2,column=2)
+b1.grid(row=4, column=1, columnspan=2)
+a_label.grid(row=5,column=1)
+a_entry.grid(row=5,column=2)
+
+
+
+
+win.mainloop()
